0275a97ad9c73d7439d9dd066636f3a61aad5c30
SaiAkshay456 <saiakshayorg@gmail.com>
2025-01-23 17:02:55 +0530

    added url mongo

--- database/db.js
+++ database/db.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 
 export const connectToDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            dbName: "CAREER_MATE"
-        })
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("connected to mongodb");
     }
     catch (err) {
-        console.log("An error occurred while connecting mongodb..")
+        console.log("An error occurred while connecting mongodb.." + err.message)
     }
 }
 
