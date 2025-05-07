7489b2adbc94fa79e64935ada8ea244fde714c89
SaiAkshay456 <saiakshayorg@gmail.com>
2025-05-08 00:32:13 +0530

    added frontend url

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dotenv.config()
 // Local frontend
 // "https://careermateapp.netlify.app"// Deployed frontend
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://careermateai.netlify.app/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
 }));
